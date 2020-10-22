75ff2a41d08404bd377d99a639d50efd26742956
marco <marclandes@free.fr>
2020-10-22 16:28:17 +0200

    new app created : apss/blink for tests purpose only

--- apps/blink/src/main.c
+++ apps/blink/src/main.c
@@ -23,9 +23,20 @@
 
 #include "bsp/bsp.h"
 #include "hal/hal_gpio.h"
-#define EXT_OUTPUT          MCU_GPIO_PORTC(5)
-#define BLINK_PERIOD        5000
 
+#define EXT_OUTPUT          MCU_GPIO_PORTC(5)
+#define BLINK_PERIOD        os_time_ms_to_ticks32(2500)
+
+#define TIMER_WITH_CALLOUT
+//#define TIMER_WITH_CPU
+//#define BLOCKING_TIMER
+
+#ifdef TIMER_WITH_CPU
+#include <hal/hal_timer.h>
+struct hal_timer timer;
+#endif
+
+#ifdef TIMER_WITH_CALLOUT
 static struct os_callout blink_work;
 
 void blink_extio(struct os_event *work){
@@ -33,7 +44,15 @@ void blink_extio(struct os_event *work){
     hal_gpio_toggle(EXT_OUTPUT);
     os_callout_reset(&blink_work, BLINK_PERIOD);
 }
+#endif
 
+#ifdef TIMER_WITH_CPU
+static void
+blink_timer(void *arg){
+    hal_gpio_toggle(EXT_OUTPUT);
+    os_cputime_timer_relative(&timer, BLINK_PERIOD*1000);
+}
+#endif
 
 int
 main(int argc, char **argv)
@@ -48,12 +67,26 @@ main(int argc, char **argv)
 
     hal_gpio_init_out(EXT_OUTPUT, 0);
 
+#ifdef TIMER_WITH_CALLOUT
     os_callout_init(&blink_work, os_eventq_dflt_get(), blink_extio, NULL);
     os_callout_reset(&blink_work, BLINK_PERIOD);
+#endif
+
+#ifdef TIMER_WITH_CPU
+   os_cputime_timer_init(&timer, blink_timer, NULL);
+   os_cputime_timer_relative(&timer, BLINK_PERIOD*1000);
+#endif
+
 
     /* As the last thing, process events from default event queue. */
     while (1) {
+#ifdef BLOCKING_TIMER
+    os_time_delay(BLINK_PERIOD);
+    hal_gpio_toggle(EXT_OUTPUT);
+#endif
+#ifdef TIMER_WITH_CALLOUT
         os_eventq_run(os_eventq_dflt_get());
+#endif
     }
 
     return 0;
